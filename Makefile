a10c972ef43898d95f4e8b873596f375e548698a
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2022-06-02 00:24:43 +0900

    Classfication map.cpp, stage.h

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
-all:
-	 g++ -std=c++11 -o map map.cpp -lncurses
+CC=g++
+TARGET=snakegame
+OBJECTS= map.cpp main.cpp item.cpp gate.cpp
+#  gate.cpp item.cpp
+
+all:$(TARGET)
+
+$(TARGET):$(OBJECTS)
+	$(CC) -std=c++11 -o $(TARGET) $(OBJECTS) -lncurses
+
+# all:
+# 	 g++ -std=c++11 -o main main.cpp map.cpp item.cpp -lncurses
--- map.h
+++ map.h
@@ -1,13 +1,22 @@
+#pragma once
+#include "item.h"
+#include "stage.h"
+#include "gate.h"
 #include <ncurses.h>
 #include <clocale>
-#include "stage.h"
+#include <locale.h>
+using namespace std;
 
-WINDOW *window_map, *window_mission, *window_score;
-int num_of_stage = 0;
-void init_window();
-void update_map();
-void update_mission();
-void update_score();
-void del_win();
-
-
+class Map {
+    private:
+        WINDOW *window_map, *window_mission, *window_score;
+        int current_size;
+        int goal_size;
+    public:
+        void init_window();
+        Stage update_map(Stage s);
+        void update_mission();
+        void update_score();
+        void del_win();
+        void play();
+};
