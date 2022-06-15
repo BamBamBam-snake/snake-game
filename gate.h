d3745cba5e2eb87ecca73da968184615be2471eb
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2022-06-15 20:48:47 +0900

    delete unused function

--- gate.h
+++ gate.h
@@ -7,10 +7,8 @@ using namespace std;
 
 class Gate{
     public:
-        int collision_gate();
         Stage generate_gate(Stage s);
         Stage delete_gate(Stage s);
         int gate1_row, gate1_col, gate2_row, gate2_col;
         int gate_position[2][2];
 };
-// vector<int> gate;
