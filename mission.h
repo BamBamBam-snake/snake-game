f33609ab1fc1b28d1a458c380995f950c1f39874
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2022-06-16 06:14:51 +0900

    Modify cur_mission[4] true -> false

--- mission.h
+++ mission.h
@@ -14,7 +14,7 @@ class Mission{
 
 		int score[4] = {3,0,0,0};
 		bool stageClear = false;
-		bool cur_mission[4] = {false,false,false,true};
+		bool cur_mission[4] = {false,false,false,false};
 		int goal_mission[4][4] = {{6,5,2,1},{7,6,3,2},{8,7,4,3},{9,8,5,4}};
 		bool goalStage[4] = {false,false,false,false};
 
