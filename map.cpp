a1473dcac3de2e7c71971225bc262d018ff2ccef
JinMyeong-Kim-Kr <kjmdkdlel@gmail.com>
2022-06-15 20:47:13 +0900

    Modify window_mission,window_score position

--- map.cpp
+++ map.cpp
@@ -11,11 +11,11 @@ void Map:: init_window(){
     // @window_map: x = 15, y = 15 위치에 가로 40, 세로 30의 크기의 윈도우
     window_map = newwin(30, 40, 15, 15);
     // @window_mission: 미션 표시 윈도우
-    window_mission = newwin(10, 30, 5, 15);
+    window_mission = newwin(10, 30, 15, 57);
     // @window_score: 점수 표시 윈도우
-    window_score = newwin(10, 30, 5, 57);
+    window_score = newwin(10, 30, 25, 57);
     // @window_status: 게임 상태 표시 윈도우
-    window_status = newwin(5, 30, 15, 57);
+    window_status = newwin(5, 30, 40, 57);
 
     // 윈도우 테두리 출력
     wborder(window_status, '|', '|', '-', '-', '+', '+', '+', '+');
